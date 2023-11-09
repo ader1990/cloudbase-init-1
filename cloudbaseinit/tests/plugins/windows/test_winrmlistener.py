6c7c205a392c57f6786206e557844c28dbb1057d
Adrian Vladu <avladu@cloudbasesolutions.com>
2023-11-09 14:09:46 +0000

    test_winrmlistener: fix unit tests for py 3.12

--- cloudbaseinit/tests/plugins/windows/test_winrmlistener.py
+++ cloudbaseinit/tests/plugins/windows/test_winrmlistener.py
@@ -112,12 +112,8 @@ class ConfigWinRMListenerPluginTests(unittest.TestCase):
                 [mock.call(6, 0), mock.call(6, 2)])
             (mock_security_utils.get_uac_remote_restrictions.
              assert_called_once_with())
-            if disable_uac_remote_restrictions:
-                expected_set_token_calls = [mock.call(enable=True)]
-            else:
-                expected_set_token_calls = [mock.call(enable=False),
-                                            mock.call(enable=True)]
-            mock_security_utils.set_uac_remote_restrictions.has_calls(
+            expected_set_token_calls = [mock.call(enable=False)]
+            mock_security_utils.set_uac_remote_restrictions.assert_has_calls(
                 expected_set_token_calls)
 
     def test_check_uac_remote_restrictions(self):
